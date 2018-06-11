e24c7928ee05cfe937ffd6b1b32e326f2d4a5795
Anastasia Garelli <anastasia.garelli@gmail.com>
2018-06-11 16:16:41 -0400

    Completed Split/Broken Merge

---
+++ mergesort.js
@@ -0,0 +1,33 @@
+function split(wholeArr) {
+
+  if (wholeArr.length <= 1) {
+    return wholeArr;
+  }
+
+  let half = Math.ceil(wholeArr.length / 2);
+  let left = wholeArr.slice(0, half);
+  let right = wholeArr.slice(half, wholeArr.length);
+
+  wholeArr = [left, right];
+
+  return wholeArr;
+}
+
+function merge(left, right) {
+  let temp = [];
+
+  if (left.length <= 1) {
+    temp.push(left);
+  }
+
+  for (var i = 0; i < left.length; i++) {
+    console.log(i);
+    if (left[i] <= right[i]) {
+      temp.push(left[i]);
+    } else {
+      temp.push(right[i]);
+    }
+
+  }
+  return temp;
+}
---
+++ mergesort.spec.js
@@ -0,0 +1,20 @@
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    let testArr = [1,2,3,4,5,6]
+    expect(split(testArr)).toEqual([[ 1, 2, 3 ], [ 4, 5, 6]]);
+  });
+});
+
+describe('Split Array function', function() {
+  it('is able to split an array into two halves if it has an odd length', function() {
+    let testArr = [1,2,3,4,5]
+    expect(split(testArr)).toEqual([[ 1, 2, 3 ], [ 4, 5]]);
+  });
+});
+
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    let testArr = [5, 6, 8, 1, 3, 2, 7, 4];
+    expect(merge(testArr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+});
